5bfd95ea16b174f00661095b73b022d66520d8ea
JoseWilton <josewilltton@gmail.com>
2019-08-29 22:25:37 -0300

    seletor iframe

---
+++ ruby/capy/spec/iframes_spec.rb
@@ -0,0 +1,35 @@
+
+
+describe 'iframes' do
+
+    describe 'iframe bom', :nice do
+        before(:each) do
+            visit '/nice_iframe'
+        end
+
+        it 'adicionar ao carrinho' do
+            within_frame('burgerId') do
+                produto = find('.menu-item-info-box', text: 'REFRIGERANTE')
+                produto.find('a').click
+                
+                expect(find('#cart')).to have_content 'R$ 4,50'
+            end
+        end
+    end
+
+    describe 'iframe_ruim', :ruim do
+        before(:each) do
+            visit '/bad_iframe'
+        end
+
+        it 'carrinho deve estar vazio' do
+            script = '$(".box-iframe").attr("id", "tempId");'
+
+            page.execute_script(script)
+
+            within_frame('tempId') do
+                expect(find('#cart')).to have_content 'Seu carrinho está vazio!'
+            end
+        end
+    end
+end
